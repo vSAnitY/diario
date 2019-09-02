82720809358ecf65cebf5703cfd245514750ea9f
vSAnitY_- <rsolis2@ucol.mx>
2019-09-02 09:57:11 -0500

    xxx

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = cbAlimentos.SelectedIndex;
+            label1.Text = indice.ToString();
+            label2.Text = cbAlimentos.Items[indice].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbAlimentos.Items.Add("Pozole");
+            cbAlimentos.Items.Add("Tostadas de Pata");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cbAlimentos.Items.Add(textBox1.Text);
+        }
+    }
+}
